f0d395033fdf866c529d11fa63a1519912b54a2d
Сергей <sergei.mamaev@gmail.com>
2024-06-21 16:42:21 +0300

    /

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
-<script setup>
+<template>
+  <header class="header">
+    <img src="/logo.svg" alt="logo" class="header-logo">
+    <h1>My Favorite Movies</h1>
+  </header>
+  <div class="movies">
+    <h3>All movies</h3>
+    <Movie></Movie>
+    {{movieStore.movies}}
+  </div>
 
+</template>
+<script setup>
+import Movie from './components/Movie.vue'
+import {useMovieStore} from './stores/MovieStore'
+const movieStore=useMovieStore();
 </script>
 
-<template>
-  
-</template>
 
-<style scoped>
 
+<style lang="css">
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+.header-logo {
+  max-width: 50px;
+  margin-right: 10px;
+}
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+.btn:hover {
+  opacity: 0.7;
+}
+.btn_green {
+  background: #37df5c;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
 </style>
